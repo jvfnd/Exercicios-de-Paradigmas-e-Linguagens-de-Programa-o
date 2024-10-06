841f367624dbb2269e13a21f5da17aa82d352dbe
João Vitor <113381115+jvfnd@users.noreply.github.com>
2024-10-05 21:32:55 -0300

    Create Carro.java

---
+++ Questao-1/Java/Classes/Carro.java
@@ -0,0 +1,39 @@
+package Classes;
+
+public class Carro{
+  private String marca;
+  private String modelo;
+  private int ano;
+
+
+  public void InformaCarro(){
+    System.out.println("Marca: "+marca+" Modelo: "+modelo+" Ano: "+ano+"\n");
+  }
+
+  public Carro(String marca, String modelo, int ano){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+  }
+
+  public String getMarca(){
+    return marca;
+  }
+  public void setMarca(String marca){
+    this.marca = marca;
+  }
+  public String getModelo(){
+    return modelo;
+  }
+  public void setModelo(String modelo){
+    this.modelo = modelo;
+  }
+  public int getAno(){
+    return ano;
+  }
+  public void setAno(int ano){
+    this.ano = ano;
+  }
+
+
+}
