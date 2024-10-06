40cd8c1f8da15a11f9a65e62a842645f98700927
João Vitor <113381115+jvfnd@users.noreply.github.com>
2024-10-06 12:25:56 -0300

    Create Main.java

---
+++ Questão-2/Java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Fiat", "Uno", 2011, 0);
+
+
+        carro.Acelerar();
+        carro.Acelerar();
+        carro.Frear();
+        carro.MostrarVelocidade();
+
+    }
+}
