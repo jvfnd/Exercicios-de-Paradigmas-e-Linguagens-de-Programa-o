3345f251f347b97f098ae952598cf87b16d4c860
João Vitor <113381115+jvfnd@users.noreply.github.com>
2024-10-07 15:58:14 -0300

    Create Empregado.java

---
+++ Questão-8/Java/empresa/Empregado.java
@@ -0,0 +1,39 @@
+package empresa;
+
+public class Empregado {
+    private String nome;
+    private String cargo;
+    private double salario;
+
+    public Empregado(String nome, String cargo,  double salario) {
+
+        this.cargo = cargo;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
+
