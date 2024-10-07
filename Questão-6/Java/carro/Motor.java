a4b30bcf7e47f340332fba1468c19943397246bd
João Vitor <113381115+jvfnd@users.noreply.github.com>
2024-10-07 12:39:36 -0300

    Create Motor.java

---
+++ Questão-6/Java/carro/Motor.java
@@ -0,0 +1,11 @@
+package carro;
+
+public class Motor {
+
+    public void Ligar(){
+        System.out.println("Motor está ligado");
+    }
+    public void Desligar(){
+        System.out.println("Motor está desligado");
+    }
+}
